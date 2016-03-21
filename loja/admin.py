3562da05a8edb542e189723f7b043c54a7693a8e
pedula <renato.pedula@sr2.uerj.br>
2016-03-20 21:56:53 -0300

    montando model do sistema

--- loja/admin.py
+++ loja/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from loja.models import Usuario, Produto, Pedido
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Produto)
+admin.site.register(Pedido)
